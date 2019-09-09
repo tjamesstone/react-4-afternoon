6afeb59715faa4225aaac3b1ff8ec7322de8d0ed
Tate Stone <tatejamesstone@gmail.com>
2019-09-09 15:18:08 -0600

    Inital Commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
+import {HashRouter} from 'react-router-dom'
 
 export default class App extends Component {
   render() {
     return (
+      <HashRouter>
       <div>
         <nav className='nav'>
-          <div>WestSide University</div> 
-          <div className='link-wrap'>
-              <div className='links'>Home</div>
-              <div className='links'>About</div> 
-          </div>
+            <div>WestSide University</div> 
+            <div className='link-wrap'>
+                <div className='links'>Home</div>
+                <div className='links'>About</div> 
+            </div>
         </nav>
       </div>
+      </HashRouter>
     )
   }
 }
